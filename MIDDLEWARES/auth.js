96e283aa9d60eb42ddff9b369f7a48f1549a8139
Shubham Singh Parihar <130331638+saulgoodman0111@users.noreply.github.com>
2023-12-10 22:27:13 +0530

    Update auth.js

--- MIDDLEWARES/auth.js
+++ MIDDLEWARES/auth.js
@@ -5,7 +5,7 @@ const User=require('../MODELS/User');
 const auth = async (req, res, next) => {
   try {
       const token = req.header('Authorization').replace('Bearer ', '');
-      // dasfasgasgsagfasg
+     
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
       const user = await User.findOne({
           _id: decoded._id,
